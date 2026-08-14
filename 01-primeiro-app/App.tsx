acbe883b36c1c596c17398194998c2611f5e9df1
Miguel Augusto <miguelaugustorocha2008@gmail.com>
2026-08-14 09:27:31 -0300

    Anotações

--- 01-primeiro-app/App.tsx
+++ 01-primeiro-app/App.tsx
@@ -1,24 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// A função "export default" significa que não precisa chamá-la posteriormente"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello World!</Text>
+      <Text style={styles.text}>Primeiro App!</Text>
+      <StatusBar style="auto" /> {/* Barra de status (Bateria, Horário) */}
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, // Deixa a caixa flexível, ocupa espaço (Teste apagar).
+    backgroundColor: '#0f0',
+    alignItems: 'center', // Centralizando horizontalmente
+    justifyContent: 'center', // Centralizando verticlamente
   },
   text: {
-    fontSize: 20,
-    left: 20
+    fontSize: 20
   }
 }); 
