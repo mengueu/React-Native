7e1451b5c8c2bce2cf033f7a3e762a6226600d8a
Miguel Augusto <miguelaugustorocha2008@gmail.com>
2026-08-14 11:22:18 -0300

    Tipos de Layout: Relativo (posicionamento absoluto, um elemento em relação a outro)

--- 01-primeiro-app/App.tsx
+++ 01-primeiro-app/App.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// Importação da biblioteca
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Tela() {
   return (
-    <SafeAreaView>
-      <View style={{ flexDirection: 'column' }}>
-        <Text>Item 1</Text>
-        <Text>Item 2</Text>
-        <Text>Item 3</Text>
-      </View>
-
-      <View style={{ flexDirection: 'row' }}>
-        <Text>Item 1</Text>
-        <Text>Item 2</Text>
-        <Text>Item 3</Text>
-      </View>
-    </SafeAreaView>
+      <SafeAreaView style={ styles.container }>
+        <Text> Tela com um Botão Flutuante</Text>
+        <View style={ styles.botao_flutuante }>
+          <Text style={ styles.texto }>+</Text>
+        </View>
+      </SafeAreaView>    
   );
 }
+
+const styles = StyleSheet.create({
+	container: { 
+		flex: 1
+	},
+	botao_flutuante: {
+	  position: 'absolute',
+	  bottom: 20,
+	  right: 20,
+	  backgroundColor: '#4A90D9',
+	  borderRadius: 30,
+	  padding: 16,
+	},
+	texto: {
+		color: '#fff'
+	}
+});
